cb28c35d1edae7240f0b1118eb4d4d4c4e1fc3ea
ygor emmanoel <ygoremmanoel23@gmail.com>
2025-04-15 10:12:52 -0300

    media aritimetica

---
+++ Arquivo07.py
@@ -0,0 +1,7 @@
+soma=0
+numero = int(input("Digite as nota aritimeticas:"))
+for x in range(numero):
+    numero1 = float(input("Digite as nota aritimeticas:"))
+    soma=soma+numero1
+media=soma/numero
+print(media)
